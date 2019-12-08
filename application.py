abe20db12c1246cd364dbfa341f2b9554e868fb4
yek273 <yejin_kim@college.harvard.edu>
2019-12-08 22:11:35 +0000

    changes

--- application.py
+++ application.py
@@ -3,7 +3,11 @@ import os
 import folium
 from folium import plugins
 
+#we added the folium extension - they had to be downloaded as a package - such that we could use the interactive map function
+
 import pandas as pd
+
+#panda is also part of the interactive map
 
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
@@ -13,7 +17,6 @@ from werkzeug.exceptions import default_exceptions, HTTPException, InternalServe
 from werkzeug.security import check_password_hash, generate_password_hash
 
 todos = []
-actualyear = 0
 
 app = Flask(__name__)
 
@@ -31,42 +34,45 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+#the folium map is centered in a certain location; we choose the location information for Japan and start with a zoomed out versionn
 folium_map = folium.Map(location= [34.6937, 135.5023], zoom_start= 7)
 
+#our mainpage displays the basic information of our project
 @app.route("/", methods = ["GET"])
 def choose_std():
     return render_template("standard.html")
 
+#datatable provides us with what information we used
 @app.route("/dtable", methods = ["GET"])
 def choose_dtable():
     return render_template("dtable.html")
 
-@app.route("/year", methods = ["GET", "POST"])
+# this page allows to choose the tourist information per year
+@app.route("/year", methods = ["GET"])
 def template():
-    if request.method == "GET":
-        return render_template("tourism.html")
-    else:
-        tyear = request.form.get("year")
-        actualyear = int(tyear)
-        print(actualyear)
-        return redirect("/map")
+    return render_template("tourism.html")
 
+#The following is coding for interactive map with data overlay
 @app.route("/map2017", methods = ["GET"])
 def resultmap2017():
+    # we first, import the csv of the data that we need; they were collected and cleaned in advance
     film_data = pd.read_csv("film.csv")
     film_data = film_data.dropna()
     film_data.head()
 
+    #we use a for loop to generate red icons that demonstrate where movies were shot
     for index, row in film_data.iterrows():
         folium.Marker(
             location=[float(row['Longitude']),float(row['Latitude'])],
             popup = row['Film'],
             icon=folium.Icon(color='red', icon='info-sign')).add_to(folium_map)
 
+    #same cleaned, organized data which will be used
     tourist_data = pd.read_csv("Visit Rate Ranking.csv")
     tourist_data = tourist_data.dropna()
     tourist_data.head()
 
+    #we use a for loop to generate circle overlays that will demonstrate the proportion of tourists per district
     for index, row in tourist_data.iterrows():
         folium.CircleMarker(
             location=[float(row['Longitude']), float(row['Latitude'])],
