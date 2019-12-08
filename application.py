9bb36f599d6148abb9e37ffa95ea5b98ffbc6690
Olivia Fu <oliviafu@college.harvard.edu>
2019-12-08 21:23:58 +0000

    merge

--- application.py
+++ application.py
@@ -30,7 +30,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-folium_parameters = { 'location': [34.6937, 135.5023], 'zoom_start' : 5, 'width': 1200, 'height': 1200}
+folium_parameters = { 'location': [34.6937, 135.5023], 'zoom_start' : 5, 'width': 1200, 'height': 1200 }
 folium_map = folium.Map(**folium_parameters)
 
 @app.route("/", methods = ["GET"])
@@ -44,27 +44,6 @@ def template():
     else:
         tyear = request.form.get("year")
         print(tyear)
-        for index, row in tourist_data.iterrows():
-            folium.CircleMarker(
-                location=[float(row['Longitude']), float(row['Latitude'])],
-                popup=row['Prefecture'],
-                radius=3*row[2017],
-                color='#3186cc',
-                fill=True,
-                fill_color='#3186cc').add_to(folium_map)
-        folium_map.save("templates/my_map.html")
-
-@app.route("/map", methods = ["GET"])
-def resultmap():
-    film_data = pd.read_csv("film.csv")
-    film_data = film_data.dropna()
-    film_data.head()
-
-    for index, row in film_data.iterrows():
-        folium.Marker(
-            location=[float(row['Longitude']),float(row['Latitude'])],
-            popup = row['Film'],
-            icon=folium.Icon(color='red', icon='info-sign')).add_to(folium_map)
 
     tourist_data = pd.read_csv("Visit Rate Ranking.csv")
     tourist_data = tourist_data.dropna()
