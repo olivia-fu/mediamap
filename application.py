d852fe0b9d9b376f9d161237ff381141dfa5ea3c
yek273 <yejin_kim@college.harvard.edu>
2019-12-08 21:23:25 +0000

    all kinds of things

--- application.py
+++ application.py
@@ -14,6 +14,8 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 todos = []
 
+actualyear = 0
+
 app = Flask(__name__)
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -30,8 +32,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-folium_parameters = { 'location': [34.6937, 135.5023], 'zoom_start' : 5, 'width': 1200, 'height': 1200}
-folium_map = folium.Map(**folium_parameters)
+folium_map = folium.Map(location= [34.6937, 135.5023], zoom_start= 7)
 
 @app.route("/", methods = ["GET"])
 def choose_std():
@@ -43,16 +44,45 @@ def template():
         return render_template("tourism.html")
     else:
         tyear = request.form.get("year")
-        print(tyear)
-        for index, row in tourist_data.iterrows():
-            folium.CircleMarker(
-                location=[float(row['Longitude']), float(row['Latitude'])],
-                popup=row['Prefecture'],
-                radius=3*row[2017],
-                color='#3186cc',
-                fill=True,
-                fill_color='#3186cc').add_to(folium_map)
-        folium_map.save("templates/my_map.html")
+        actualyear = tyear
+
+        # if tyear == "2016":
+        #     for index, row in tourist_data.iterrows():
+        #         folium.CircleMarker(
+        #             location=[float(row['Longitude']), float(row['Latitude'])],
+        #             popup=row[tyear],
+        #             radius=3*row[tyear],
+        #             color='#cc3131',
+        #             fill=True if while == "2016",
+        #             fill_color='#cc3131').add_to(folium_map)
+        #     folium_map.save("templates/my_map.html")
+        #     return render_template("my_map.html")
+
+        # if tyear == "2015":
+        #     for index, row in tourist_data.iterrows():
+        #         folium.CircleMarker(
+        #             location=[float(row['Longitude']), float(row['Latitude'])],
+        #             popup=row[tyear],
+        #             radius=3*row[tyear],
+        #             color='#31cc55',
+        #             fill=True while tyear == "2015",
+        #             fill_color='#31cc55').add_to(folium_map)
+        #     folium_map.save("templates/my_map.html")
+        #     return render_template("my_map.html")
+
+        # if tyear == "2014":
+        #     for index, row in tourist_data.iterrows():
+        #         folium.CircleMarker(
+        #             location=[float(row['Longitude']), float(row['Latitude'])],
+        #             popup=row[tyear],
+        #             radius=3*row[tyear],
+        #             color='#cc31b5',
+        #             fill=True while tyear == "2014",
+        #             fill_color='#cc31b5').add_to(folium_map)
+        #     folium_map.save("templates/my_map.html")
+        # folium_map.save("templates/my_map.html")
+        return render_template("my_map.html")
+        #     return render_template("my_map.html")
 
 @app.route("/map", methods = ["GET"])
 def resultmap():
@@ -70,13 +100,37 @@ def resultmap():
     tourist_data = tourist_data.dropna()
     tourist_data.head()
 
-    return render_template("my_map.html")
+    if actualyear == 2017:
+        for index, row in tourist_data.iterrows():
+            folium.CircleMarker(
+                location=[float(row['Longitude']), float(row['Latitude'])],
+                popup=row['Prefecture'],
+                radius=3*row['2017'],
+                color='#3186cc',
+                fill=True,
+                fill_color='#3186cc').add_to(folium_map)
+        folium_map.save("templates/my_map.html")
+        return render_template("my_map.html")
 
-@app.route("/request", methods=["GET", "POST"])
+    if actualyear == 2016:
+        for index, row in tourist_data.iterrows():
+            folium.CircleMarker(
+                location=[float(row['Longitude']), float(row['Latitude'])],
+                popup=row['Prefecture'],
+                radius=3*row['2016'],
+                color='#cc3131',
+                fill=True,
+                fill_color='#cc3131').add_to(folium_map)
+        folium_map.save("templates/my_map.html")
+        return render_template("my_map.html")
+
+
+@app.route("/request", methods=["GET"])
 def moreinfo():
     if request.method == "GET":
         return render_template("request.html",todos = todos)
     else:
-        comment = request.form.get("comment")
-        todos.append(comment)
+        todo = request.form.get("comment")
+        print(todo)
+        todos.append(todo)
         return render_template("request.html")
